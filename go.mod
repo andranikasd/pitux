7a7f5ba801edcc65515c14324e7c1f48c687a6ba
Andranik Grigoryan <andranik.grigoryan.personal@gmail.com>
2024-10-20 23:01:39 +0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module andranik.me/pitux
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("This fucking tool will find any Armenian dude you are searching for")
+}
